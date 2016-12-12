d1512f62d919d7c3b84e8414283b2c01e237fc10
Arshdeep Singh & Evan Renaud & Melanie Gershman & Sherianne Bolling <pair+arsy209.evanrenaud.melaniegershman.sheriannebolling@devbootcamp.com>
2016-12-12 16:40:22 -0500

    seed movies from OMDB API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# mel = User.create(username: "mel", email: "sadgirl@gmail.com", password: "password")
+# jaws = Movie.create(genre: "comedy", title: "jaws", year: "1989", director: "tom", cast: "some guys", plot: "sharks")
+# fave1 = Favorite.create(user_id: 1, movie_id: 1)
+# review1 = Review.create(user_id: 1, movie_id: 1, comment: "idk", rating: 3)
+# rate2 = Review.create(user_id: 1, movie_id: 1, comment: "lol", rating: 5)
+base_id = "0107289"
+1000.times do |time|
+  id = base_id.to_i
+  id += time
+  request = "http://www.omdbapi.com/?i=tt0#{id.to_s}"
+  query = open(request).read
+  response = JSON.parse(query)
+  unless response["Response"] == "False"
+    Movie.create(genre: response["Genre"].split(", ").first, title: response["Title"], year: response["Year"], director: response["Director"], cast: response["Actors"], plot: response["Plot"])
+  end
+end
---
+++ spec/controllers/movie_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MovieController, type: :controller do
+
+end
