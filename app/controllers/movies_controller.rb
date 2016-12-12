82f7c41a58e8e1876239b2bc2861c03b23a94caa
Arshdeep Singh & Evan Renaud & Melanie Gershman & Sherianne Bolling <pair+arsy209.evanrenaud.melaniegershman.sheriannebolling@devbootcamp.com>
2016-12-12 17:28:41 -0500

    Sort movies by genre

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    #  @movies = Movie.paginate(page: params[:page], per_page: 20)
+    @movies = Movie.where(genre: params[:genre])
   end
 
   def show
