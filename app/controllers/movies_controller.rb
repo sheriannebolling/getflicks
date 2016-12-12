36499267302b81aa0ff7d9d02c30d5c22de7e639
Arshdeep Singh & Evan Renaud & Melanie Gershman & Sherianne Bolling <pair+arsy209.evanrenaud.melaniegershman.sheriannebolling@devbootcamp.com>
2016-12-12 17:31:23 -0500

    Take out paginate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 class MoviesController < ApplicationController
   def index
     # @movies = Movie.all
-    #  @movies = Movie.paginate(page: params[:page], per_page: 20)
-    @movies = Movie.where(genre: params[:genre])
+
+    @movies = Movie.where(genre: params[:genre].capitalize)
   end
 
   def show
